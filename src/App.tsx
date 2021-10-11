a4b15dc6f6d88931d8186fa8b571ee3fb6c02c4f
Prashanth Vengala <prashanth.vengala@perennialsys.com>
2021-10-11 11:21:23 +0530

    Updated the changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import './App.css';
+import { Routes } from './Routes/Routes';
 
 
 function App() {
   return (
     <>
-    Setting up the project
+    <Routes/>
     </>
   );
 }
